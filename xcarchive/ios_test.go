89cd8ce3fcdacc4d7b8616b1ed0ef7d636220e42
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-11-16 14:19:41 +0100

    xcarchive package fixes

--- xcarchive/ios_test.go
+++ xcarchive/ios_test.go
@@ -1,6 +1,7 @@
 package xcarchive
 
 import (
+	"fmt"
 	"path/filepath"
 	"testing"
 
@@ -9,15 +10,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var tmpDir = ""
+
+func sampleRepoPath(t *testing.T) string {
+	dir := ""
+	if tmpDir != "" {
+		dir = tmpDir
+	} else {
+		var err error
+		dir, err = pathutil.NormalizedOSTempDirPath("__artifacts__")
+		require.NoError(t, err)
+		sampleArtifactsGitURI := "https://github.com/bitrise-samples/sample-artifacts.git"
+		cmd := command.New("git", "clone", sampleArtifactsGitURI, dir)
+		require.NoError(t, cmd.Run())
+		tmpDir = dir
+	}
+	t.Logf("sample artifcats dir: %s\n", dir)
+	return dir
+}
+
 func TestNewIosArchive(t *testing.T) {
-	sampleArtifactsGitURI := "https://github.com/bitrise-samples/sample-artifacts.git"
-	tmpDir, err := pathutil.NormalizedOSTempDirPath("__artifacts__")
-	require.NoError(t, err)
-
-	cmd := command.New("git", "clone", sampleArtifactsGitURI, tmpDir)
-	require.NoError(t, cmd.Run())
-
-	iosArchivePth := filepath.Join(tmpDir, "archives/ios.xcarchive")
+	iosArchivePth := filepath.Join(sampleRepoPath(t), "archives/ios.xcarchive")
 	archive, err := NewIosArchive(iosArchivePth)
 	require.NoError(t, err)
 	require.Equal(t, 5, len(archive.InfoPlist))
@@ -44,4 +57,61 @@ func TestNewIosArchive(t *testing.T) {
 	require.Equal(t, 23, len(watchExtension.InfoPlist))
 	require.Equal(t, 2, len(watchExtension.Entitlements))
 	require.Equal(t, "*", watchExtension.ProvisioningProfile.BundleID)
+}
+
+func TestIsXcodeManaged(t *testing.T) {
+	iosArchivePth := filepath.Join(sampleRepoPath(t), "archives/ios.xcarchive")
+	archive, err := NewIosArchive(iosArchivePth)
+	require.NoError(t, err)
+
+	require.Equal(t, false, archive.IsXcodeManaged())
+}
+
+func TestSigningIdentity(t *testing.T) {
+	iosArchivePth := filepath.Join(sampleRepoPath(t), "archives/ios.xcarchive")
+	archive, err := NewIosArchive(iosArchivePth)
+	require.NoError(t, err)
+
+	require.Equal(t, "iPhone Developer: Bitrise Bot (VV2J4SV8V4)", archive.SigningIdentity())
+}
+
+func TestBundleIDEntitlementsMap(t *testing.T) {
+	iosArchivePth := filepath.Join(sampleRepoPath(t), "archives/ios.xcarchive")
+	archive, err := NewIosArchive(iosArchivePth)
+	require.NoError(t, err)
+
+	bundleIDEntitlementsMap := archive.BundleIDEntitlementsMap()
+	require.Equal(t, 4, len(bundleIDEntitlementsMap))
+
+	bundleIDs := []string{"com.bitrise.code-sign-test.share-extension", "com.bitrise.code-sign-test.watchkitapp", "com.bitrise.code-sign-test.watchkitapp.watchkitextension", "com.bitrise.code-sign-test"}
+	for _, bundleID := range bundleIDs {
+		_, ok := bundleIDEntitlementsMap[bundleID]
+		require.True(t, ok, fmt.Sprintf("%v", bundleIDEntitlementsMap))
+	}
+}
+
+func TestBundleIDProfileInfoMap(t *testing.T) {
+	iosArchivePth := filepath.Join(sampleRepoPath(t), "archives/ios.xcarchive")
+	archive, err := NewIosArchive(iosArchivePth)
+	require.NoError(t, err)
+
+	bundleIDProfileInfoMap := archive.BundleIDProfileInfoMap()
+	require.Equal(t, 4, len(bundleIDProfileInfoMap))
+
+	bundleIDs := []string{"com.bitrise.code-sign-test.share-extension", "com.bitrise.code-sign-test.watchkitapp", "com.bitrise.code-sign-test.watchkitapp.watchkitextension", "com.bitrise.code-sign-test"}
+	for _, bundleID := range bundleIDs {
+		_, ok := bundleIDProfileInfoMap[bundleID]
+		require.True(t, ok, fmt.Sprintf("%v", bundleIDProfileInfoMap))
+	}
+}
+
+func TestFindDSYMs(t *testing.T) {
+	iosArchivePth := filepath.Join(sampleRepoPath(t), "archives/ios.xcarchive")
+	archive, err := NewIosArchive(iosArchivePth)
+	require.NoError(t, err)
+
+	appDsym, otherDsyms, err := archive.FindDSYMs()
+	require.NoError(t, err)
+	require.NotEmpty(t, appDsym)
+	require.Equal(t, 2, len(otherDsyms))
 }
--- xcarchive/macos_test.go
+++ xcarchive/macos_test.go
@@ -1,23 +1,15 @@
 package xcarchive
 
 import (
+	"fmt"
 	"path/filepath"
 	"testing"
 
-	"github.com/bitrise-io/go-utils/command"
-	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewMacosArchive(t *testing.T) {
-	sampleArtifactsGitURI := "https://github.com/bitrise-samples/sample-artifacts.git"
-	tmpDir, err := pathutil.NormalizedOSTempDirPath("__artifacts__")
-	require.NoError(t, err)
-
-	cmd := command.New("git", "clone", sampleArtifactsGitURI, tmpDir)
-	require.NoError(t, cmd.Run())
-
-	macosArchivePth := filepath.Join(tmpDir, "archives/macos.xcarchive")
+	macosArchivePth := filepath.Join(sampleRepoPath(t), "archives/macos.xcarchive")
 	archive, err := NewMacosArchive(macosArchivePth)
 	require.NoError(t, err)
 	require.Equal(t, 5, len(archive.InfoPlist))
@@ -32,4 +24,55 @@ func TestNewMacosArchive(t *testing.T) {
 	require.Equal(t, 22, len(extension.InfoPlist))
 	require.Equal(t, 2, len(extension.Entitlements))
 	require.Nil(t, extension.ProvisioningProfile)
+}
+
+func TestMacosIsXcodeManaged(t *testing.T) {
+	macosArchivePth := filepath.Join(sampleRepoPath(t), "archives/macos.xcarchive")
+	archive, err := NewMacosArchive(macosArchivePth)
+	require.NoError(t, err)
+
+	require.Equal(t, false, archive.IsXcodeManaged())
+}
+
+func TestMacosSigningIdentity(t *testing.T) {
+	macosArchivePth := filepath.Join(sampleRepoPath(t), "archives/macos.xcarchive")
+	archive, err := NewMacosArchive(macosArchivePth)
+	require.NoError(t, err)
+
+	require.Equal(t, "Mac Developer: Gödrei Krisztian (T3694PR6UJ)", archive.SigningIdentity())
+}
+
+func TestMacosBundleIDEntitlementsMap(t *testing.T) {
+	macosArchivePth := filepath.Join(sampleRepoPath(t), "archives/macos.xcarchive")
+	archive, err := NewMacosArchive(macosArchivePth)
+	require.NoError(t, err)
+
+	bundleIDEntitlementsMap := archive.BundleIDEntitlementsMap()
+	require.Equal(t, 2, len(bundleIDEntitlementsMap))
+
+	bundleIDs := []string{"io.bitrise.archive.Test", "io.bitrise.archive.Test.ActionExtension"}
+	for _, bundleID := range bundleIDs {
+		_, ok := bundleIDEntitlementsMap[bundleID]
+		require.True(t, ok, fmt.Sprintf("%v", bundleIDEntitlementsMap))
+	}
+}
+
+func TestMacosBundleIDProfileInfoMap(t *testing.T) {
+	macosArchivePth := filepath.Join(sampleRepoPath(t), "archives/macos.xcarchive")
+	archive, err := NewMacosArchive(macosArchivePth)
+	require.NoError(t, err)
+
+	bundleIDProfileInfoMap := archive.BundleIDProfileInfoMap()
+	require.Equal(t, 0, len(bundleIDProfileInfoMap))
+}
+
+func TestMacosFindDSYMs(t *testing.T) {
+	macosArchivePth := filepath.Join(sampleRepoPath(t), "archives/macos.xcarchive")
+	archive, err := NewMacosArchive(macosArchivePth)
+	require.NoError(t, err)
+
+	appDsym, otherDsyms, err := archive.FindDSYMs()
+	require.NoError(t, err)
+	require.NotEmpty(t, appDsym)
+	require.Equal(t, 1, len(otherDsyms))
 }
