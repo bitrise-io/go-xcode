a75a0ebdabce4b1f973d1a50c15aa4be02b75145
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-11-20 14:04:57 +0100

    removed none export method (#53)

--- exportoptions/properties.go
+++ exportoptions/properties.go
@@ -79,8 +79,6 @@ func (manifest Manifest) ToHash() map[string]string {
 const MethodKey = "method"
 
 const (
-	// MethodNone ...
-	MethodNone Method = "none"
 	// MethodAppStore ...
 	MethodAppStore Method = "app-store"
 	// MethodAdHoc ...
@@ -116,7 +114,7 @@ func ParseMethod(method string) (Method, error) {
 	case "developer-id":
 		return MethodDeveloperID, nil
 	default:
-		return MethodNone, fmt.Errorf("unkown method (%s)", method)
+		return Method(""), fmt.Errorf("unkown method (%s)", method)
 	}
 }
 
