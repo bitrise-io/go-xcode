642e6cce080b3c59478ddbc87f3e1485c2b8fd84
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-02-25 16:06:02 +0100

    Optimixed custom escape helper.

--- xcarchive/glob.go
+++ xcarchive/glob.go
@@ -1,11 +1,12 @@
 package xcarchive
 
-import "strings"
-
 func escapeGlobPath(path string) string {
-	path = strings.Replace(path, "\\", "\\\\", -1)
-	for _, char := range []string{"[", "*", "?"} {
-		path = strings.Replace(path, char, "\\"+char, -1)
+	var escaped string
+	for _, ch := range path {
+		if ch == '[' || ch == '*' || ch == '?' || ch == '\\' {
+			escaped += "\\"
+		}
+		escaped += string(ch)
 	}
-	return path
+	return escaped
 }
