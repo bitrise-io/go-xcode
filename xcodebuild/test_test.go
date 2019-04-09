b9dae7d1ec0e5e6158abadf6de8781dde0562272
Akos Birmacher <akos.birmacher@bitrise.io>
2019-04-09 11:01:22 +0200

    test: add test for xcodebuild/test.go

---
+++ xcodebuild/test_test.go
@@ -0,0 +1,161 @@
+package xcodebuild
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestTestCommandModel_cmdSlice(t *testing.T) {
+	tests := []struct {
+		name                 string
+		projectPath          string
+		isWorkspace          bool
+		scheme               string
+		destination          string
+		generateCodeCoverage bool
+		indexWhileBuilding   bool
+		customBuildActions   []string
+		customOptions        []string
+		want                 []string
+	}{
+		{
+			name:                 "test simulator",
+			projectPath:          "ios/project.xcprojec",
+			isWorkspace:          false,
+			scheme:               "project",
+			destination:          "id 2323asd2s",
+			generateCodeCoverage: false,
+			indexWhileBuilding:   false,
+			customBuildActions:   []string{""},
+			customOptions:        []string{""},
+			want: []string{
+				"xcodebuild",
+				"-project",
+				"ios/project.xcprojec",
+				"-scheme",
+				"project",
+				"",
+				"test",
+				"-destination",
+				"id 2323asd2s",
+				"COMPILER_INDEX_STORE_ENABLE=NO",
+				"",
+			},
+		},
+		{
+			name:                 "test generate code coverage",
+			projectPath:          "ios/project.xcprojec",
+			isWorkspace:          false,
+			scheme:               "project",
+			destination:          "id 2323asd2s",
+			generateCodeCoverage: true,
+			indexWhileBuilding:   false,
+			customBuildActions:   []string{""},
+			customOptions:        []string{""},
+			want: []string{
+				"xcodebuild",
+				"-project",
+				"ios/project.xcprojec",
+				"-scheme",
+				"project",
+				"GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES",
+				"GCC_GENERATE_TEST_COVERAGE_FILES=YES",
+				"",
+				"test",
+				"-destination",
+				"id 2323asd2s",
+				"COMPILER_INDEX_STORE_ENABLE=NO",
+				"",
+			},
+		},
+		{
+			name:                 "test workspace",
+			projectPath:          "ios/project.xcworkspaxe",
+			isWorkspace:          true,
+			scheme:               "project",
+			destination:          "id 2323asd2s",
+			generateCodeCoverage: false,
+			indexWhileBuilding:   false,
+			customBuildActions:   []string{""},
+			customOptions:        []string{""},
+			want: []string{
+				"xcodebuild",
+				"-workspace",
+				"ios/project.xcworkspaxe",
+				"-scheme",
+				"project",
+				"",
+				"test",
+				"-destination",
+				"id 2323asd2s",
+				"COMPILER_INDEX_STORE_ENABLE=NO",
+				"",
+			},
+		},
+		{
+			name:                 "test generic iOS",
+			projectPath:          "ios/project.xcproject",
+			isWorkspace:          false,
+			scheme:               "project",
+			destination:          "generic/platform=iOS",
+			generateCodeCoverage: false,
+			indexWhileBuilding:   false,
+			customBuildActions:   []string{""},
+			customOptions:        []string{""},
+			want: []string{
+				"xcodebuild",
+				"-project",
+				"ios/project.xcproject",
+				"-scheme",
+				"project",
+				"",
+				"test",
+				"-destination",
+				"generic/platform=iOS",
+				"COMPILER_INDEX_STORE_ENABLE=NO",
+				"",
+			},
+		},
+		{
+			name:                 "test generic iOS index while building",
+			projectPath:          "ios/project.xcprojec",
+			isWorkspace:          false,
+			scheme:               "project",
+			destination:          "id 2323asd2s",
+			generateCodeCoverage: false,
+			indexWhileBuilding:   true,
+			customBuildActions:   []string{""},
+			customOptions:        []string{""},
+			want: []string{
+				"xcodebuild",
+				"-project",
+				"ios/project.xcprojec",
+				"-scheme",
+				"project",
+				"",
+				"test",
+				"-destination",
+				"id 2323asd2s",
+				"",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &TestCommandModel{
+				projectPath:          tt.projectPath,
+				isWorkspace:          tt.isWorkspace,
+				scheme:               tt.scheme,
+				destination:          tt.destination,
+				generateCodeCoverage: tt.generateCodeCoverage,
+				indexWhileBuilding:   tt.indexWhileBuilding,
+				customBuildActions:   tt.customBuildActions,
+				customOptions:        tt.customOptions,
+			}
+			if got := c.cmdSlice(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TestCommandModel.cmdSlice() = %v\nwant %v", strings.Join(got, "\n"), strings.Join(tt.want, "\n"))
+			}
+		})
+	}
+}
