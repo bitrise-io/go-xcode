58833faa79ba5098a2056e6cf60f5b59cd22167d
lpusok <7979773+lpusok@users.noreply.github.com>
2019-07-12 16:59:00 +0200

    Tool 574 support multiple identities in p12 (#83)
    * expose CertificateInfos's Certificate and PrivateKey (if instantiated from .p12 file) property
    
    * Support multiple keypairs in p12
    
    Adding EncodeTo12 receiver for CertificateInfoModel
    
    * dep update
    
    * dep update
    
    * Return invalid and duplicated certificates from FilterValidCertificateInfos
    
    * Exporting ValidCertifiacteInfo fields.
    
    * Fixing EncodeToP12
    
    * Exporting ValidCertificateInfo fields
    
    * Printing CertificateInfoModel as a string in one line
    
    * Adding  new line in CertificateInfoModel.String()
    
    * Removing new line
    
    * Use new DecodeAll function from pkcs12 package
    
    * Fix issue that not all duplicated certs are returned, adding tests for FilterValidCertificateInfos
    
    * Removing NewCertificateInfosFromPKCS12
    
    * dep update
    
    * IMprove FilterValidCertificateInfos readibility
    
    * dep update

--- certificateutil/filter_test.go
+++ certificateutil/filter_test.go
@@ -1,8 +1,10 @@
 package certificateutil
 
 import (
+	"reflect"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -36,4 +38,76 @@ func TestFilterCertificateInfoModelsByFilterFunc(t *testing.T) {
 
 	foundCerts = FilterCertificateInfoModelsByFilterFunc(filterableCerts, func(cert CertificateInfoModel) bool { return strings.Contains(cert.CommonName, "common name") })
 	require.Equal(t, expectedCertsByCommonNameMatch, foundCerts)
+}
+
+func TestFilterValidCertificateInfos(t *testing.T) {
+	const serial = int64(1234)
+	const teamID = "MYTEAMID"
+	const teamName = "BITFALL FEJLESZTO KORLATOLT FELELOSSEGU TARSASAG"
+	const commonName = "Apple Developer: test"
+	validExpiry := time.Now().AddDate(1, 0, 0)
+	earlierValidExpiry := time.Now().AddDate(0, 1, 0)
+	invalidExpiry := time.Now().AddDate(-1, 0, 0)
+
+	latestValidCert, privateKey, err := GenerateTestCertificate(serial, teamID, teamName, commonName, validExpiry)
+	if err != nil {
+		t.Errorf("init: failed to generate certificate, error: %s", err)
+	}
+	latestValidCertInfo := NewCertificateInfo(*latestValidCert, privateKey)
+	t.Logf("Test certificate generated: %s", latestValidCertInfo)
+
+	earlierValidCert, privateKey, err := GenerateTestCertificate(serial, teamID, teamName, commonName, earlierValidExpiry)
+	if err != nil {
+		t.Errorf("init: failed to generate certificate, error: %s", err)
+	}
+	earlierValidCertInfo := NewCertificateInfo(*latestValidCert, earlierValidCert)
+	t.Logf("Test certificate generated: %s", earlierValidCertInfo)
+
+	invalidCert, privateKey, err := GenerateTestCertificate(serial, teamID, teamName, commonName, invalidExpiry)
+	if err != nil {
+		t.Errorf("init: failed to generate certificate, error: %s", err)
+	}
+	invalidCertInfo := NewCertificateInfo(*invalidCert, privateKey)
+	t.Logf("Test certificate generated: %s", invalidCertInfo)
+
+	tests := []struct {
+		name             string
+		certificateInfos []CertificateInfoModel
+		want             ValidCertificateInfo
+	}{
+		{
+			name:             "one valid cert",
+			certificateInfos: []CertificateInfoModel{latestValidCertInfo},
+			want: ValidCertificateInfo{
+				ValidCertificates:      []CertificateInfoModel{latestValidCertInfo},
+				InvalidCertificates:    nil,
+				DuplicatedCertificates: nil,
+			},
+		},
+		{
+			name:             "one valid, one invalid cert with same name",
+			certificateInfos: []CertificateInfoModel{latestValidCertInfo, invalidCertInfo},
+			want: ValidCertificateInfo{
+				ValidCertificates:      []CertificateInfoModel{latestValidCertInfo},
+				InvalidCertificates:    []CertificateInfoModel{invalidCertInfo},
+				DuplicatedCertificates: nil,
+			},
+		},
+		{
+			name:             "2 valid, duplicated certs",
+			certificateInfos: []CertificateInfoModel{latestValidCertInfo, earlierValidCertInfo, invalidCertInfo},
+			want: ValidCertificateInfo{
+				ValidCertificates:      []CertificateInfoModel{latestValidCertInfo},
+				InvalidCertificates:    []CertificateInfoModel{invalidCertInfo},
+				DuplicatedCertificates: []CertificateInfoModel{earlierValidCertInfo},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FilterValidCertificateInfos(tt.certificateInfos); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FilterValidCertificateInfos() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
