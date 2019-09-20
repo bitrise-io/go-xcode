24dbaeda539ddec3a4bbe11ac4e92d434e21f9ef
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-09-20 12:26:06 +0200

    Adding cache package, used to cache Swift PM packages

---
+++ cache/derived_data_path_test.go
@@ -0,0 +1,76 @@
+package cache
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/bitrise-io/go-utils/pathutil"
+)
+
+func Test_xcodeProjectHash(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "normal xcodeproj path",
+			path:    "/Users/lpusok/Develop/samples/sample-apps-ios-swiftpm/sample-swiftpm.xcodeproj",
+			want:    "atfutdtkzefhykgeccaarxqthpih",
+			wantErr: false,
+		},
+		{
+			name:    "normal xcworkspace path",
+			path:    "/Users/lpusok/Develop/samples/sample-apps-ios-swiftpm/sample-swiftpm.xcworkspace",
+			want:    "ffgdnmiynjdztkbwkockhhlizbxd",
+			wantErr: false,
+		},
+		{
+			name:    "Unicode composite character in path",
+			path:    "/Users/lpusok/Develop/samples/Gdańsk/sample-swiftpm.xcodeproj",
+			want:    "ehewgchenmpwjkgtcvczfaxgbjuv",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := xcodeDerivedDataHash(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("xcodeProjectHash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("xcodeProjectHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_xcodeProjectDerivedDataPath(t *testing.T) {
+	tests := []struct {
+		name        string
+		projectPath string
+		want        string
+		wantErr     bool
+	}{
+		{
+			name:        "normal xcodeproj path",
+			projectPath: "/Users/lpusok/Develop/samples/sample-apps-ios-swiftpm/sample-swiftpm.xcodeproj",
+			want:        filepath.Join(pathutil.UserHomeDir(), "Library/Developer/Xcode/DerivedData/sample-swiftpm-atfutdtkzefhykgeccaarxqthpih"),
+			wantErr:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := xcodeProjectDerivedDataPath(tt.projectPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("xcodeProjectDerivedDataPath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("xcodeProjectDerivedDataPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
