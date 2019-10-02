39758b99f466fbd5271280a29e55d6982e0de8bd
lpusok <7979773+lpusok@users.noreply.github.com>
2019-10-02 15:54:48 +0200

    Tool 1126 swiftpm cache (#85)
    * Adding cache package, used to cache Swift PM packages
    
    * dep update
    
    * Renaming cache package to xcodecache
    
    * Exclude manifest.db from swift cache
    
    * Exclude path
    
    * Adding cache descriptor file
    
    * Adding periods to comments, use bitrise as username in test data
    
    * Remove swift cache indicator file, as is not an officially documented one.

---
+++ xcodecache/derived_data_path_test.go
@@ -0,0 +1,76 @@
+package cache
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/bitrise-io/go-utils/pathutil"
+)
+
+func Test_xcodeProjectHash(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "normal xcodeproj path",
+			path:    "/Users/bitrise/git/sample-swiftpm.xcodeproj",
+			want:    "dsvyrfhmubmjkdguolhekiuetuie",
+			wantErr: false,
+		},
+		{
+			name:    "normal xcworkspace path",
+			path:    "/Users/bitrise/Develop/samples/sample-apps-ios-swiftpm/sample-swiftpm.xcworkspace",
+			want:    "domyjojidpnjraaljgmxofiwqhps",
+			wantErr: false,
+		},
+		{
+			name:    "Unicode composite character in path",
+			path:    "/Users/bitrise/Develop/samples/Gdańsk/sample-swiftpm.xcodeproj",
+			want:    "djfhdbzbhhqfklgywrqyqnyflvnl",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := xcodeDerivedDataHash(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("xcodeProjectHash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("xcodeProjectHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_xcodeProjectDerivedDataPath(t *testing.T) {
+	tests := []struct {
+		name        string
+		projectPath string
+		want        string
+		wantErr     bool
+	}{
+		{
+			name:        "normal xcodeproj path",
+			projectPath: "/Users/bitrise/Develop/samples/sample-apps-ios-swiftpm/sample-swiftpm.xcodeproj",
+			want:        filepath.Join(pathutil.UserHomeDir(), "Library/Developer/Xcode/DerivedData/sample-swiftpm-asravewhgfsybjdvvequtqgqhbea"),
+			wantErr:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := xcodeProjectDerivedDataPath(tt.projectPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("xcodeProjectDerivedDataPath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("xcodeProjectDerivedDataPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
