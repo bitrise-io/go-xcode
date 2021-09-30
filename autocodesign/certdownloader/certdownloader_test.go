d89c768d156aaf34d6d7927930b88e5b761f4b7d
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-09-30 11:23:08 +0200

    adding certdownlaoder test

---
+++ autocodesign/certdownloader/certdownloader_test.go
@@ -0,0 +1,117 @@
+package certdownloader
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/bitrise-io/go-xcode/certificateutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_downloader_GetCertificates_Local(t *testing.T) {
+	const teamID = "MYTEAMID"
+	const commonName = "Apple Developer: test"
+	const teamName = "BITFALL FEJLESZTO KORLATOLT FELELOSSEGU TARSASAG"
+	expiry := time.Now().AddDate(1, 0, 0)
+	serial := int64(1234)
+
+	cert, privateKey, err := certificateutil.GenerateTestCertificate(serial, teamID, teamName, commonName, expiry)
+	if err != nil {
+		t.Errorf("init: failed to generate certificate: %s", err)
+	}
+
+	certInfo := certificateutil.NewCertificateInfo(*cert, privateKey)
+	t.Logf("Test certificate generated. Serial: %s Team ID: %s Common name: %s", certInfo.Serial, certInfo.TeamID, certInfo.CommonName)
+
+	passphrase := ""
+	certData, err := certInfo.EncodeToP12(passphrase)
+	if err != nil {
+		t.Errorf("init: failed to encode certificate: %s", err)
+	}
+
+	p12File, err := ioutil.TempFile("", "*.p12")
+	if err != nil {
+		t.Errorf("init: failed to create temp test file: %s", err)
+	}
+
+	if _, err = p12File.Write(certData); err != nil {
+		t.Errorf("init: failed to write test file: %s", err)
+	}
+
+	if err = p12File.Close(); err != nil {
+		t.Errorf("init: failed to close file: %s", err)
+	}
+
+	p12path := "file://" + p12File.Name()
+
+	d := downloader{
+		urls: []CertificateAndPassphrase{{
+			URL:        p12path,
+			Passphrase: passphrase,
+		}},
+		client: http.DefaultClient,
+	}
+	got, err := d.GetCertificates()
+
+	want := []certificateutil.CertificateInfoModel{
+		certInfo,
+	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
+
+func Test_downloader_GetCertificates_Remote(t *testing.T) {
+	const teamID = "MYTEAMID"
+	const commonName = "Apple Developer: test"
+	const teamName = "BITFALL FEJLESZTO KORLATOLT FELELOSSEGU TARSASAG"
+	expiry := time.Now().AddDate(1, 0, 0)
+	serial := int64(1234)
+
+	cert, privateKey, err := certificateutil.GenerateTestCertificate(serial, teamID, teamName, commonName, expiry)
+	if err != nil {
+		t.Errorf("init: failed to generate certificate: %s", err)
+	}
+
+	certInfo := certificateutil.NewCertificateInfo(*cert, privateKey)
+	t.Logf("Test certificate generated. Serial: %s Team ID: %s Common name: %s", certInfo.Serial, certInfo.TeamID, certInfo.CommonName)
+
+	passphrase := ""
+	certData, err := certInfo.EncodeToP12(passphrase)
+	if err != nil {
+		t.Errorf("init: failed to encode certificate: %s", err)
+	}
+
+	storage := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write(certData)
+		if err != nil {
+			t.Errorf("failed to write response: %s", err)
+		}
+	}))
+
+	//
+	d := downloader{
+		urls: []CertificateAndPassphrase{{
+			URL:        storage.URL,
+			Passphrase: passphrase,
+		}},
+		client: http.DefaultClient,
+	}
+	got, err := d.GetCertificates()
+
+	want := []certificateutil.CertificateInfoModel{
+		certInfo,
+	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
