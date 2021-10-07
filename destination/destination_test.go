93e3a53643a24af229fd5ed957d1f1b49c57f843
Bence Zsigmond Nagy <Bence1001@users.noreply.github.com>
2021-10-07 14:51:22 +0200

    Migrate duplicated logic from steps (#119)
    * Removed unnecessary xcodebuild version check
    
    * Added destination package
    
    * Added tests for NewSimulator
    
    * Added xcconfig package

---
+++ destination/destination_test.go
@@ -0,0 +1,86 @@
+package destination
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_GivenDestinationIsCorrect_WhenSimulatorIsCreated_ThenItReturnsCorrectSimulator(t *testing.T) {
+	// Given
+	destination := "platform=iOS Simulator,name=iPhone 8 Plus,OS=latest"
+
+	// When
+	simulator, err := NewSimulator(destination)
+
+	// Then
+	if assert.NoError(t, err) {
+		expectedSimulator := &Simulator{Platform: "iOS Simulator", Name: "iPhone 8 Plus", OS: "latest"}
+		assert.Equal(t, expectedSimulator, simulator)
+	}
+}
+
+func Test_GivenDestinationHasNoOS_WhenSimulatorIsCreated_ThenItReturnsSimulatorWithLatestOS(t *testing.T) {
+	// Given
+	destination := "platform=iOS Simulator,name=iPhone 8 Plus"
+
+	// When
+	simulator, err := NewSimulator(destination)
+
+	// Then
+	if assert.NoError(t, err) {
+		expectedSimulator := &Simulator{Platform: "iOS Simulator", Name: "iPhone 8 Plus", OS: "latest"}
+		assert.Equal(t, expectedSimulator, simulator)
+	}
+}
+
+func Test_GivenDestinationHasNoPlatform_WhenSimulatorIsCreated_ThenItReturnsAnError(t *testing.T) {
+	// Given
+	destination := "name=iPhone 8 Plus,OS=latest"
+
+	// When
+	simulator, err := NewSimulator(destination)
+
+	// Then
+	if assert.Error(t, err) {
+		assert.Nil(t, simulator)
+	}
+}
+
+func Test_GivenDestinationHasNoName_WhenSimulatorIsCreated_ThenItReturnsAnError(t *testing.T) {
+	// Given
+	destination := "platform=iOS Simulator,OS=latest"
+
+	// When
+	simulator, err := NewSimulator(destination)
+
+	// Then
+	if assert.Error(t, err) {
+		assert.Nil(t, simulator)
+	}
+}
+
+func Test_GivenDestinationHasInvalidKey_WhenSimulatorIsCreated_ThenItReturnsAnError(t *testing.T) {
+	// Given
+	destination := "invalid=iOS Simulator,name=iPhone 8 Plus,OS=latest"
+
+	// When
+	simulator, err := NewSimulator(destination)
+
+	// Then
+	if assert.Error(t, err) {
+		assert.Nil(t, simulator)
+	}
+}
+
+func Test_GivenDestinationHasInvalidFormat_WhenSimulatorIsCreated_ThenItReturnsAnError(t *testing.T) {
+	// Given
+	destination := "platform:iOS Simulator,name:iPhone 8 Plus,OS:latest"
+
+	// When
+	simulator, err := NewSimulator(destination)
+
+	// Then
+	if assert.Error(t, err) {
+		assert.Nil(t, simulator)
+	}
+}
