7b03b9347e710a57dabd0345d6453fa2bbf4c9c3
Olivér Falvai <oliver.falvai@bitrise.io>
2021-10-11 08:40:21 -0700

    Remove unused builder methods (#121)
    * Remove unused builder methods
    
    * Remove unnecessary test cases

--- xcodebuild/build_test.go
+++ xcodebuild/build_test.go
@@ -73,17 +73,6 @@ func TestCommandBuilder_cmdSlice(t *testing.T) {
 			},
 		},
 		{
-			name: "force development team",
-			builder: CommandBuilder{
-				forceDevelopmentTeam: "Bitrise",
-				action:               BuildAction,
-			},
-			want: []string{
-				"DEVELOPMENT_TEAM=Bitrise",
-				"build",
-			},
-		},
-		{
 			name: "force code sign identity",
 			builder: CommandBuilder{
 				forceCodeSignIdentity: "iOS Developer: Bitrise bot",
@@ -91,19 +80,6 @@ func TestCommandBuilder_cmdSlice(t *testing.T) {
 			},
 			want: []string{
 				"CODE_SIGN_IDENTITY=iOS Developer: Bitrise bot",
-				"build",
-			},
-		},
-		{
-			name: "force profile",
-			builder: CommandBuilder{
-				forceProvisioningProfileSpecifier: "id 2331232",
-				forceProvisioningProfile:          "id 2331232",
-				action:                            BuildAction,
-			},
-			want: []string{
-				"PROVISIONING_PROFILE_SPECIFIER=id 2331232",
-				"PROVISIONING_PROFILE=id 2331232",
 				"build",
 			},
 		},
@@ -154,17 +130,6 @@ func TestCommandBuilder_cmdSlice(t *testing.T) {
 				"archive",
 				"-archivePath",
 				"archive/path",
-			},
-		},
-		{
-			name: "disable index while building",
-			builder: CommandBuilder{
-				disableIndexWhileBuilding: true,
-				action:                    BuildAction,
-			},
-			want: []string{
-				"COMPILER_INDEX_STORE_ENABLE=NO",
-				"build",
 			},
 		},
 		{
