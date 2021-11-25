812121ffac782859642a9cf0a0301a3115bc7caf
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-11-25 16:34:57 +0100

    Adding exported comments

--- autocodesign/errors.go
+++ autocodesign/errors.go
@@ -54,10 +54,12 @@ func (e NonmatchingProfileError) Error() string {
 	return fmt.Sprintf("provisioning profile does not match requirements: %s", e.Reason)
 }
 
+// ProfilesInconsistentError is returned when a profile is deleted by an other actor
 type ProfilesInconsistentError struct {
 	wrapErr error
 }
 
+// NewProfilesInconsistentError ...
 func NewProfilesInconsistentError(wrapErr error) ProfilesInconsistentError {
 	return ProfilesInconsistentError{
 		wrapErr: wrapErr,
