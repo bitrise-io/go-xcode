81e1195a25f12c46eaca877566ec992467144d4e
godrei <krisztian.godrei@bitrise.io>
2021-12-01 09:16:09 +0100

    Revert projectmanager changes

--- autocodesign/projectmanager/projectmanager.go
+++ autocodesign/projectmanager/projectmanager.go
@@ -81,12 +81,6 @@ func (p Project) MainTargetBundleID() (string, error) {
 
 // GetAppLayout ...
 func (p Project) GetAppLayout(uiTestTargets bool) (autocodesign.AppLayout, error) {
-	isSigningManagedAutomatically, err := p.IsSigningManagedAutomatically()
-	if err != nil {
-		return autocodesign.AppLayout{}, fmt.Errorf("failed to check if 'Automatically manage signing' enabled: %s", err)
-	}
-	log.Printf("Automatically manage signing: %v", isSigningManagedAutomatically)
-
 	log.Printf("Configuration: %s", p.projHelper.Configuration)
 
 	teamID, err := p.projHelper.ProjectTeamID(p.projHelper.Configuration)
