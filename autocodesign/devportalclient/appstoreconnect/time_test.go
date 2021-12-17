abbf21968ec9e94427d655715bcd21fde4079cf3
Szabolcs Toth <szabolcs.toth@bitrise.io>
2021-12-17 11:22:35 +0000

    Better ISO 8601 time parsing

--- autocodesign/devportalclient/appstoreconnect/time_test.go
+++ autocodesign/devportalclient/appstoreconnect/time_test.go
@@ -1,6 +1,8 @@
 package appstoreconnect
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTime_UnmarshalJSON(t *testing.T) {
 
@@ -10,8 +12,15 @@ func TestTime_UnmarshalJSON(t *testing.T) {
 		t       *Time
 		wantErr bool
 	}{
-		{name: "without quotation mark", b: []byte("2021-05-19T08:07:47.000+0000"), wantErr: false},
-		{name: "with quotation mark", b: []byte(`"2021-05-19T08:07:47.000+0000"`), wantErr: false},
+		{name: "without quotation mark", b: []byte("2021-05-19T08:07:47.000+00:00"), wantErr: false},
+		{name: "with quotation mark", b: []byte(`"2021-05-19T08:07:47.000+00:00"`), wantErr: false},
+		{name: "Positive offset", b: []byte("2021-05-19T08:07:47.000+05:30"), wantErr: false},
+		{name: "Alternate positive offset", b: []byte("2021-05-19T08:07:47.000+0530"), wantErr: false},
+		{name: "Alternate negative offset", b: []byte("2021-05-19T08:07:47.000-0400"), wantErr: false},
+		{name: "Single hour positive offset", b: []byte("2021-05-19T08:07:47.000+03"), wantErr: false},
+		{name: "Single hour negative offset", b: []byte("2021-05-19T08:07:47.000-02"), wantErr: false},
+		{name: "Zero offset UTC", b: []byte("2021-05-19T08:07:47.000Z"), wantErr: false},
+		{name: "unsupported format", b: []byte("2021-12-17T10:44:00Z00:00"), wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
