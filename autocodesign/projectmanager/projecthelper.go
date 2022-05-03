612ec4da70c246e08347b86f106602c01d79b9a2
Olivér Falvai <oliver.falvai@bitrise.io>
2022-05-03 09:43:47 +0200

    Target parsing refactors, part 2.

--- autocodesign/projectmanager/projecthelper.go
+++ autocodesign/projectmanager/projecthelper.go
@@ -54,11 +54,12 @@ func NewProjectHelper(projOrWSPath, schemeName, configurationName string) (*Proj
 		return nil, fmt.Errorf("failed to find the main target of the scheme (%s): %s", schemeName, err)
 	}
 
-	dependentTargets := mainTarget.DependentExecutableProductTargets()
+	//dependentTargets := mainTarget.DependentExecutableProductTargets()
+	dependentTargets := xcproj.DependentTargetsOfTarget(mainTarget) // TODO: filtering
 
 	var uiTestTargets []xcodeproj.Target
 	for _, target := range xcproj.Proj.Targets {
-		if target.IsUITestProduct() && target.DependesOn(mainTarget.ID) {
+		if target.IsUITestProduct() && target.DependsOn(mainTarget.ID) {
 			uiTestTargets = append(uiTestTargets, target)
 		}
 	}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/bitrise-io/go-steputils/v2 v2.0.0-alpha.1
 	github.com/bitrise-io/go-utils v1.0.1
 	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.1
-	github.com/bitrise-io/go-xcode v1.0.1
+	github.com/bitrise-io/go-xcode v1.0.6-0.20220502132536-bfa0553e6b09
 	github.com/golang-jwt/jwt/v4 v4.2.0
 	github.com/google/go-querystring v1.1.0
 	github.com/hashicorp/go-retryablehttp v0.7.0
