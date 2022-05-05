2f1d787b2a8ffa10eebebc375c443535a22e3e43
Sujit Poudel <sujitpoudel123@gmail.com>
2022-05-05 11:59:20 -0400

    Add comment, and arrange the test code with comments

--- xcconfig/xcconfig_test.go
+++ xcconfig/xcconfig_test.go
@@ -31,6 +31,7 @@ func Test_WhenWritingXCConfigContent_ThenItShouldReturnFilePath(t *testing.T) {
 }
 
 func Test_XCConfigInput_NonExistentPathErrors(t *testing.T) {
+	// Given
 	testContent := "TEST.xcconfig"
 	testTempDir := "temp_dir"
 	expectedPath := filepath.Join(testTempDir, "temp.xcconfig")
@@ -41,20 +42,28 @@ func Test_XCConfigInput_NonExistentPathErrors(t *testing.T) {
 	mockPathChecker := new(mocks.PathChecker)
 	mockPathChecker.On("IsPathExists", testContent).Return(false, errors.New("path does not exist"))
 	xcconfigWriter := NewWriter(mockPathProvider, mockFileManager, mockPathChecker)
+
+	// When
 	path, err := xcconfigWriter.Write(testContent)
+
+	// Then
 	assert.Error(t, err)
 	assert.Equal(t, path, "")
 }
 
 func Test_XCConfigInput_CorrectInputPathReturnSamePath(t *testing.T) {
+	// Given
 	input := "TEST.xcconfig"
 	mockPathChecker := new(mocks.PathChecker)
 	mockPathChecker.On("IsPathExists", input).Return(true, nil)
 	mockPathProvider := new(mocks.PathProvider)
 	mockFileManager := new(mocks.FileManager)
 	xcconfigWriter := NewWriter(mockPathProvider, mockFileManager, mockPathChecker)
+
+	// When
 	path, err := xcconfigWriter.Write(input)
 
+	// Then
 	if assert.NoError(t, err) {
 		assert.Equal(t, path, input)
 	}
