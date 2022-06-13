600183635828325b5297c47a41c365d6a5947332
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2022-06-13 20:57:14 +0200

    Add Test Plan support to Scheme (#179)
    * Add Test Plan support to Scheme
    
    * Add test plan support to xcodebuild package
    
    * Replace scheme content variables with actual scheme files
    
    * Rename scheme tests
    
    * Restore TestScheme_Marshal test case

--- xcodeproject/xcscheme/xcscheme_test.go
+++ xcodeproject/xcscheme/xcscheme_test.go
@@ -1,47 +1,74 @@
 package xcscheme
 
 import (
-	"encoding/xml"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 
-	"github.com/bitrise-io/go-xcode/xcodeproject/testhelper"
 	"github.com/stretchr/testify/require"
 )
 
-func TestOpenScheme(t *testing.T) {
-	pth := testhelper.CreateTmpFile(t, "ios-simple-objc.xcscheme", schemeContent)
-	scheme, err := Open(pth)
+func Test_GivenScheme_WhenMarshal_ThenContentRemain(t *testing.T) {
+	// Given
+	schemePth := "testdata/ios-simple-objc.xcscheme"
+
+	f, err := os.Open(schemePth)
+	require.NoError(t, err)
+
+	scheme, err := parse(f)
+	require.NoError(t, err)
+
+	// When
+	content, err := scheme.Marshal()
+
+	// Then
+	require.NoError(t, err)
+
+	_, err = f.Seek(0, io.SeekStart)
+	require.NoError(t, err)
+
+	schemeContent, err := ioutil.ReadAll(f)
+	require.NoError(t, err)
+	require.Equal(t, schemeContent, content)
+}
+
+func Test_GivenSchemeWithTestPlan_WhenOpen_ThenDefaultTestPlanSet(t *testing.T) {
+	// Given
+	schemePth := "testdata/BullsEye.xcscheme"
+
+	// When
+	scheme, err := Open(schemePth)
+
+	// Then
+	require.NoError(t, err)
+
+	testPlan := scheme.DefaultTestPlan()
+	require.NotNil(t, testPlan)
+	require.Equal(t, "FullTests", testPlan.Name())
+}
+
+func Test_GivenSimpleScheme_WhenOpen(t *testing.T) {
+	// Given
+	schemePth := "testdata/ios-simple-objc.xcscheme"
+
+	// When
+	scheme, err := Open(schemePth)
+
+	// Then
 	require.NoError(t, err)
 
 	require.Equal(t, "ios-simple-objc", scheme.Name)
-	require.Equal(t, pth, scheme.Path)
+	require.Equal(t, schemePth, scheme.Path)
 
 	require.Equal(t, "Release", scheme.ArchiveAction.BuildConfiguration)
 	require.Equal(t, 2, len(scheme.BuildAction.BuildActionEntries))
 
-	{
-		entry := scheme.BuildAction.BuildActionEntries[0]
-		require.Equal(t, "YES", entry.BuildForArchiving)
-		require.Equal(t, "YES", entry.BuildForTesting)
-		require.Equal(t, "BA3CBE7419F7A93800CED4D5", entry.BuildableReference.BlueprintIdentifier)
-
-		pth, err := entry.BuildableReference.ReferencedContainerAbsPath("/project.xcodeproj")
-		require.NoError(t, err)
-		require.Equal(t, "/project.xcodeproj/ios-simple-objc.xcodeproj", pth)
-	}
-
-	{
-		entry := scheme.BuildAction.BuildActionEntries[1]
-		require.Equal(t, "NO", entry.BuildForArchiving)
-		require.Equal(t, "YES", entry.BuildForTesting)
-		require.Equal(t, "BA3CBE9019F7A93900CED4D5", entry.BuildableReference.BlueprintIdentifier)
-	}
+	assertIosSimpleObjCSchemeAppBuildActionEntry(scheme, t)
+	assertIosSimpleObjCSchemeTestActionEntry(scheme, t)
 }
 
-func TestAppBuildActionEntry(t *testing.T) {
-	var scheme Scheme
-	require.NoError(t, xml.Unmarshal([]byte(schemeContent), &scheme))
-
+func assertIosSimpleObjCSchemeAppBuildActionEntry(scheme Scheme, t *testing.T) {
 	entry, ok := scheme.AppBuildActionEntry()
 	require.True(t, ok)
 
@@ -52,157 +79,22 @@ func TestAppBuildActionEntry(t *testing.T) {
 	require.Equal(t, "ios-simple-objc", entry.BuildableReference.BlueprintName)
 	require.Equal(t, "container:ios-simple-objc.xcodeproj", entry.BuildableReference.ReferencedContainer)
 
+	pth, err := entry.BuildableReference.ReferencedContainerAbsPath("/project.xcodeproj")
+	require.NoError(t, err)
+	require.Equal(t, "/project.xcodeproj/ios-simple-objc.xcodeproj", pth)
+
 	require.True(t, entry.BuildableReference.IsAppReference())
 }
 
-func TestAppTestActionEntry(t *testing.T) {
-	var scheme Scheme
-	require.NoError(t, xml.Unmarshal([]byte(schemeContent), &scheme))
-
+func assertIosSimpleObjCSchemeTestActionEntry(scheme Scheme, t *testing.T) {
 	require.Equal(t, "Debug", scheme.TestAction.BuildConfiguration)
-	require.Equal(t, 2, len(scheme.TestAction.Testables))
+	require.Equal(t, 1, len(scheme.TestAction.Testables))
 	require.Equal(t, "NO", scheme.TestAction.Testables[0].Skipped)
-	require.Equal(t, "YES", scheme.TestAction.Testables[1].Skipped)
 	require.Equal(t, "BA3CBE9019F7A93900CED4D5", scheme.TestAction.Testables[0].BuildableReference.BlueprintIdentifier)
 
 	require.False(t, scheme.TestAction.Testables[0].BuildableReference.IsAppReference())
-	require.False(t, scheme.TestAction.Testables[1].BuildableReference.IsAppReference())
+
+	require.Nil(t, scheme.TestAction.TestPlans)
+	testPlan := scheme.DefaultTestPlan()
+	require.Nil(t, testPlan)
 }
-
-func TestScheme_Marshal(t *testing.T) {
-	pth := testhelper.CreateTmpFile(t, "ios-simple-objc.xcscheme", schemeContent)
-	scheme, err := Open(pth)
-	require.NoError(t, err)
-
-	content, err := scheme.Marshal()
-	require.NoError(t, err)
-
-	require.Equal(t, schemeContent, string(content))
-}
-
-const schemeContent = `<?xml version="1.0" encoding="UTF-8"?>
-<Scheme
-   LastUpgradeVersion = "0800"
-   version = "1.3">
-   <BuildAction
-      parallelizeBuildables = "YES"
-      buildImplicitDependencies = "YES">
-      <BuildActionEntries>
-         <BuildActionEntry
-            buildForTesting = "YES"
-            buildForRunning = "YES"
-            buildForProfiling = "YES"
-            buildForArchiving = "YES"
-            buildForAnalyzing = "YES">
-            <BuildableReference
-               BuildableIdentifier = "primary"
-               BlueprintIdentifier = "BA3CBE7419F7A93800CED4D5"
-               BuildableName = "ios-simple-objc.app"
-               BlueprintName = "ios-simple-objc"
-               ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-            </BuildableReference>
-         </BuildActionEntry>
-         <BuildActionEntry
-            buildForTesting = "YES"
-            buildForRunning = "YES"
-            buildForProfiling = "NO"
-            buildForArchiving = "NO"
-            buildForAnalyzing = "YES">
-            <BuildableReference
-               BuildableIdentifier = "primary"
-               BlueprintIdentifier = "BA3CBE9019F7A93900CED4D5"
-               BuildableName = "ios-simple-objcTests.xctest"
-               BlueprintName = "ios-simple-objcTests"
-               ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-            </BuildableReference>
-         </BuildActionEntry>
-      </BuildActionEntries>
-   </BuildAction>
-   <TestAction
-      buildConfiguration = "Debug"
-      selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
-      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
-      shouldUseLaunchSchemeArgsEnv = "YES">
-      <Testables>
-         <TestableReference
-            skipped = "NO">
-            <BuildableReference
-               BuildableIdentifier = "primary"
-               BlueprintIdentifier = "BA3CBE9019F7A93900CED4D5"
-               BuildableName = "ios-simple-objcTests.xctest"
-               BlueprintName = "ios-simple-objcTests"
-               ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-            </BuildableReference>
-         </TestableReference>
-         <TestableReference
-            skipped = "YES">
-            <BuildableReference
-               BuildableIdentifier = "primary"
-               BlueprintIdentifier = "BA4CBE9019F7A93900CED4D5"
-               BuildableName = "ios-simple-objcTests2.xctest"
-               BlueprintName = "ios-simple-objcTests2"
-               ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-            </BuildableReference>
-         </TestableReference>
-      </Testables>
-      <MacroExpansion>
-         <BuildableReference
-            BuildableIdentifier = "primary"
-            BlueprintIdentifier = "BA3CBE7419F7A93800CED4D5"
-            BuildableName = "ios-simple-objc.app"
-            BlueprintName = "ios-simple-objc"
-            ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-         </BuildableReference>
-      </MacroExpansion>
-      <AdditionalOptions>
-      </AdditionalOptions>
-   </TestAction>
-   <LaunchAction
-      buildConfiguration = "Debug"
-      selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
-      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
-      launchStyle = "0"
-      useCustomWorkingDirectory = "NO"
-      ignoresPersistentStateOnLaunch = "NO"
-      debugDocumentVersioning = "YES"
-      debugServiceExtension = "internal"
-      allowLocationSimulation = "YES">
-      <BuildableProductRunnable
-         runnableDebuggingMode = "0">
-         <BuildableReference
-            BuildableIdentifier = "primary"
-            BlueprintIdentifier = "BA3CBE7419F7A93800CED4D5"
-            BuildableName = "ios-simple-objc.app"
-            BlueprintName = "ios-simple-objc"
-            ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-         </BuildableReference>
-      </BuildableProductRunnable>
-      <AdditionalOptions>
-      </AdditionalOptions>
-   </LaunchAction>
-   <ProfileAction
-      buildConfiguration = "Release"
-      shouldUseLaunchSchemeArgsEnv = "YES"
-      savedToolIdentifier = ""
-      useCustomWorkingDirectory = "NO"
-      debugDocumentVersioning = "YES">
-      <BuildableProductRunnable
-         runnableDebuggingMode = "0">
-         <BuildableReference
-            BuildableIdentifier = "primary"
-            BlueprintIdentifier = "BA3CBE7419F7A93800CED4D5"
-            BuildableName = "ios-simple-objc.app"
-            BlueprintName = "ios-simple-objc"
-            ReferencedContainer = "container:ios-simple-objc.xcodeproj">
-         </BuildableReference>
-      </BuildableProductRunnable>
-   </ProfileAction>
-   <AnalyzeAction
-      buildConfiguration = "Debug">
-   </AnalyzeAction>
-   <ArchiveAction
-      buildConfiguration = "Release"
-      revealArchiveInOrganizer = "YES">
-   </ArchiveAction>
-</Scheme>
-`
