0c31e6b5d9f088512037ddedaa7f79235b533c0a
Laszlo Pusok <laszlo.pusok@bitrise.io>
2022-06-20 19:39:41 +0200

    Log time for benchmarking

--- destination/device_finder.go
+++ destination/device_finder.go
@@ -1,6 +1,8 @@
 package destination
 
 import (
+	"time"
+
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/log"
 )
@@ -35,6 +37,8 @@ func NewDeviceFinder(log log.Logger, commandFactory command.Factory) DeviceFinde
 
 // GetSimulator returns a Simulator matching the destination
 func (d deviceFinder) FindDevice(destination Simulator) (Device, error) {
+	start := time.Now()
+
 	if d.list == nil {
 		list, err := d.parseDeviceList()
 		if err != nil {
@@ -50,6 +54,8 @@ func (d deviceFinder) FindDevice(destination Simulator) (Device, error) {
 			d.logger.Warnf("failed to log device list: %s", err)
 		}
 	}
+
+	d.logger.TDebugf("Parsed simulator list in %s", time.Since(start).Round(time.Second))
 
 	return device, err
 }
