2f71c9c45c297f19be56f480ded72a55cf383de6
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-08-23 13:20:49 +0200

    Fix workspace settings key not found handling (#211)

--- xcodeproject/xcworkspace/schemes_test.go
+++ xcodeproject/xcworkspace/schemes_test.go
@@ -30,6 +30,28 @@ func Test_GivenNewlyGeneratedWorkspace_WhenListingSchemes_ThenReturnsTheDefaultS
 	require.Equal(t, true, actualSchemes[0].IsShared)
 }
 
+func Test_GivenNewlyGeneratedWorkspaceWithWorkspaceSettings_WhenListingSchemes_ThenReturnsTheDefaultScheme(t *testing.T) {
+	xcodeWorkspacePath := testhelper.NewlyGeneratedXcodeWorkspacePath(t)
+	workspace, err := Open(xcodeWorkspacePath)
+	require.NoError(t, err)
+
+	worksaceSettingsPth := filepath.Join(xcodeWorkspacePath, "xcshareddata/WorkspaceSettings.xcsettings")
+	require.NoError(t, fileutil.WriteStringToFile(worksaceSettingsPth, workspaceSettingsWithBuildSystemTypeOriginalContent))
+
+	schemesByContainer, err := workspace.Schemes()
+	require.NoError(t, err)
+
+	expectedSchemeName := "ios-sample"
+	var actualSchemes []xcscheme.Scheme
+	for _, schemes := range schemesByContainer {
+		actualSchemes = append(actualSchemes, schemes...)
+	}
+
+	require.Equal(t, 1, len(actualSchemes))
+	require.Equal(t, expectedSchemeName, actualSchemes[0].Name)
+	require.Equal(t, true, actualSchemes[0].IsShared)
+}
+
 func Test_GivenNewlyGeneratedWorkspaceWithAutocreateSchemesDisabled_WhenListingSchemes_ThenReturnsError(t *testing.T) {
 	xcodeWorkspacePath := testhelper.NewlyGeneratedXcodeWorkspacePath(t)
 
@@ -54,6 +76,16 @@ const workspaceSettingsWithAutocreateSchemesDisabledContent = `<?xml version="1.
 <dict>
 	<key>IDEWorkspaceSharedSettings_AutocreateContextsIfNeeded</key>
 	<false/>
+</dict>
+</plist>
+`
+
+const workspaceSettingsWithBuildSystemTypeOriginalContent = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>BuildSystemType</key>
+	<string>Original</string>
 </dict>
 </plist>
 `
