fa7a7430c51e24094edde86c9fd30ddf2ee6a99d
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-01-25 10:09:06 +0100

    Print spaceship prepare errors

--- autocodesign/devportalclient/spaceship/spaceship.go
+++ autocodesign/devportalclient/spaceship/spaceship.go
@@ -18,7 +18,6 @@ import (
 	"time"
 
 	"github.com/bitrise-io/go-steputils/v2/ruby"
-	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-xcode/appleauth"
@@ -222,11 +221,7 @@ func prepareSpaceship(cmdFactory ruby.CommandFactory) (string, error) {
 
 		output, err := cmd.RunAndReturnTrimmedCombinedOutput()
 		if err != nil {
-			if errorutil.IsExitStatusError(err) {
-				return "", fmt.Errorf("Installing bundler gem failed: %s", output)
-			}
-
-			return "", fmt.Errorf("running command failed: %s", err)
+			return "", fmt.Errorf("running command failed with error: %s, output: %s", err, output)
 		}
 	}
 
@@ -240,11 +235,7 @@ func prepareSpaceship(cmdFactory ruby.CommandFactory) (string, error) {
 
 	output, err := bundleInstallCmd.RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
-		if errorutil.IsExitStatusError(err) {
-			return "", fmt.Errorf("bundle install failed: %s", output)
-		}
-
-		return "", fmt.Errorf("running command failed: %s", err)
+		return "", fmt.Errorf("running command failed with error: %s, output: %s", err, output)
 	}
 
 	return targetDir, nil
