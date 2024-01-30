b742e10dae053d3a3bb70670ab07105a8ede6304
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-01-30 10:32:38 +0100

    Spaceship cmd (#226)
    * Retry spaceship command on 503 Service Temporarily Unavailable errors.
    
    * Log spaceship retry
    
    * Simplify shouldRetrySpaceshipCommand
    
    * Update shouldRetrySpaceshipCommand
    
    * Log retried spaceship command output and update gems
    
    * Update test names
    
    * Log if TFA session expired
    
    * Recreate spaceship commands for retry attempts
    
    * Increase waittime between retries
    
    * Print some logs with timestamp
    
    * Clear spaceship cookie before retrying
    
    * Increase retry wait time
    
    * Print spaceship prepare errors
    
    * Delete the whole Fastlane workdir between retries
    
    * Log spaceship login time frame
    
    * Decrease waittime
    
    * Update spaceship client
    
    * Run spaceship login only one time
    
    * Restore original error handling
    
    * Remove spaceship cookie delete between retries
    
    * Cleanup unused dependency import
    
    * Enabled spaceship debug logs
    
    * Set portal client
    
    * Increase wait time
    
    * Eliminate duplicated spaceship outputs in logs

---
+++ autocodesign/devportalclient/spaceship/spaceship_test.go
@@ -0,0 +1,56 @@
+package spaceship
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/bitrise-io/go-xcode/v2/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+const spaceshipTemporaryUnavailableOutput = `Apple ID authentication failed: <html>
+<head><title>503 Service Temporarily Unavailable</title></head>
+<body>
+<center><h1>503 Service Temporarily Unavailable</h1></center>
+<hr><center>Apple</center>
+</body>
+</html>`
+
+func Test_runSpaceshipCommand_retries_on_temporarily_unavailable_error(t *testing.T) {
+	cmd := new(mocks.Command)
+	cmd.On("RunAndReturnTrimmedCombinedOutput").Return(spaceshipTemporaryUnavailableOutput, errors.New("exit status 1")).Once()
+	cmd.On("RunAndReturnTrimmedCombinedOutput").Return(spaceshipTemporaryUnavailableOutput, errors.New("exit status 1")).Once()
+	cmd.On("RunAndReturnTrimmedCombinedOutput").Return("{}", nil).Once()
+
+	cmdFactory := new(mocks.RubyCommandFactory)
+	cmdFactory.On("CreateBundleExec", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(cmd)
+
+	c := &Client{
+		cmdFactory: cmdFactory,
+	}
+	out, err := c.runSpaceshipCommand("")
+	require.NoError(t, err)
+	require.Equal(t, "{}", out)
+
+	cmd.AssertExpectations(t)
+	cmdFactory.AssertExpectations(t)
+}
+
+func Test_runSpaceshipCommand_retries_only_temporarily_unavailable_error(t *testing.T) {
+	cmd := new(mocks.Command)
+	cmd.On("RunAndReturnTrimmedCombinedOutput").Return("exit status 1", errors.New("exit status 1")).Once()
+
+	cmdFactory := new(mocks.RubyCommandFactory)
+	cmdFactory.On("CreateBundleExec", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(cmd)
+
+	c := &Client{
+		cmdFactory: cmdFactory,
+	}
+	out, err := c.runSpaceshipCommand("")
+	require.EqualError(t, err, "spaceship command failed with output: exit status 1")
+	require.Equal(t, "", out)
+
+	cmd.AssertExpectations(t)
+	cmdFactory.AssertExpectations(t)
+}
