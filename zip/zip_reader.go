b76d9af5372d4ce61840505f0e6e5de2d1ea6bd3
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-02-13 15:07:19 +0100

    Use go-utils/v2/log in the zip package

--- zip/zip_reader.go
+++ zip/zip_reader.go
@@ -6,21 +6,25 @@ import (
 	"io"
 	"strings"
 
-	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/ryanuber/go-glob"
 )
 
 type Reader struct {
 	zipReader *zip.ReadCloser
+	logger    log.Logger
 }
 
-func OpenZip(archivePath string) (*Reader, error) {
+func NewReader(archivePath string, logger log.Logger) (*Reader, error) {
 	zipReader, err := zip.OpenReader(archivePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open archive %s: %w", archivePath, err)
 	}
 
-	return &Reader{zipReader: zipReader}, nil
+	return &Reader{
+		zipReader: zipReader,
+		logger:    logger,
+	}, nil
 }
 
 func (reader Reader) Close() error {
@@ -52,7 +56,7 @@ func (reader Reader) ReadFile(targetPathGlob string) ([]byte, error) {
 	}
 	defer func() {
 		if err := r.Close(); err != nil {
-			log.Warnf("failed to close archive file %s: %s", file.Name, err)
+			reader.logger.Warnf("failed to close archive file %s: %s", file.Name, err)
 		}
 	}()
 
