5b4b2d5ae36b0d81727844f36520ffd838367954
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-09-23 16:07:09 +0200

    xcodebuild factory with additional args

--- xcodebuild/xcodebuild.go
+++ xcodebuild/xcodebuild.go
@@ -23,16 +23,30 @@ const (
 )
 
 type Factory struct {
-	cmdFactory command.Factory
+	cmdFactory     command.Factory
+	additionalArgs []string
 }
 
 // NewFactory ...
-func NewFactory(envRepository env.Repository) Factory {
+func NewFactory(envRepository env.Repository, additionalArgs []string) Factory {
 	cmdFactory := command.NewFactory(envRepository)
 	return Factory{cmdFactory: cmdFactory}
 }
 
+func (factory Factory) CreateWithoutDefaultAdditionalArgs(options *CommandOptions, actions []string, buildSettings *CommandBuildSettings, additionalArgs []string, cmdOpts *command.Opts) command.Command {
+	return factory.Create(options, actions, buildSettings, additionalArgs, cmdOpts)
+}
+
 func (factory Factory) Create(options *CommandOptions, actions []string, buildSettings *CommandBuildSettings, additionalArgs []string, cmdOpts *command.Opts) command.Command {
+	defaultAdditionalArgsResult := parseAdditionalArgs(factory.additionalArgs)
+	additionalArgsResult := parseAdditionalArgs(additionalArgs)
+	mergedAdditionalArgsResult := mergeAdditionalArgs(defaultAdditionalArgsResult, additionalArgsResult)
+	mergedAdditionalArgs := mergedAdditionalArgsResult.toArgs()
+
+	return factory.Create(options, actions, buildSettings, mergedAdditionalArgs, cmdOpts)
+}
+
+func (factory Factory) create(options *CommandOptions, actions []string, buildSettings *CommandBuildSettings, additionalArgs []string, cmdOpts *command.Opts) command.Command {
 	var args []string
 	if options != nil {
 		args = append(args, options.cmdArgs()...)
@@ -42,5 +56,6 @@ func (factory Factory) Create(options *CommandOptions, actions []string, buildSe
 		args = append(args, buildSettings.cmdArgs()...)
 	}
 	args = append(args, additionalArgs...)
+
 	return factory.cmdFactory.Create(xcodebuildCmdName, args, cmdOpts)
 }
