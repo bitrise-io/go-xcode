6567db9896c9aeb4ee22caefbe78c73b7c2bc31c
LaszloP <7979773+lpusok@users.noreply.github.com>
2024-10-25 19:55:50 +0200

    step path lookup fix

--- e2e/step-manage-ios-code-signing/step.sh
+++ e2e/step-manage-ios-code-signing/step.sh
@@ -1,5 +1,7 @@
-#/bin/env bash
+#!/bin/env bash
 
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$THIS_SCRIPT_DIR" || exit
 path="$HOME/.bitrise/toolkits/go/cache/path-._e2e_step-manage-ios-code-signing"
 go build -o "$path"
 $path
