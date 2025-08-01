5ed8d542f9548383afbf9ce04acfc48f46b56cd7
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-08-01 16:06:31 +0200

    Move exportoptions to go-xcode@v2

---
+++ exportoptions/exportoptions.go
@@ -0,0 +1,29 @@
+package exportoptions
+
+import (
+	"fmt"
+
+	"howett.net/plist"
+
+	"github.com/bitrise-io/go-utils/fileutil"
+)
+
+// ExportOptions ...
+type ExportOptions interface {
+	Hash() map[string]interface{}
+	String() (string, error)
+	WriteToFile(pth string) error
+}
+
+// WritePlistToFile ...
+func WritePlistToFile(options map[string]interface{}, pth string) error {
+	plistBytes, err := plist.MarshalIndent(options, plist.XMLFormat, "\t")
+	if err != nil {
+		return fmt.Errorf("failed to marshal export options model, error: %s", err)
+	}
+	if err := fileutil.WriteBytesToFile(pth, plistBytes); err != nil {
+		return fmt.Errorf("failed to write export options, error: %s", err)
+	}
+
+	return nil
+}
