1f61b73f368df24b71f291596bbb640b9d9413b0
Balázs Hajagos <balazs.hajagos@bitrise.io>
2025-09-02 15:56:59 +0200

    Add a log interceptor
    Singles out [Bitrise ...] logs from the rest and forwards them to stdout

---
+++ loginterceptor/loginterceptor.go
@@ -0,0 +1,79 @@
+package loginterceptor
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"regexp"
+	"sync"
+)
+
+// PrefixInterceptor intercept writes: if a line begins with prefix, it will be written to
+// both writers. Partial writes without newline are buffered until a newline.
+type PrefixInterceptor struct {
+	re          *regexp.Regexp
+	intercepted io.Writer
+	original    io.Writer
+
+	// internal pipe and goroutine to scan and route
+	pr *io.PipeReader
+	pw *io.PipeWriter
+
+	// close once
+	closeOnce sync.Once
+	closeErr  error
+}
+
+// NewPrefixInterceptor returns an io.WriteCloser. Writes are based on line prefix.
+func NewPrefixInterceptor(re *regexp.Regexp, intercepted, original io.Writer) *PrefixInterceptor {
+	pr, pw := io.Pipe()
+	interceptor := &PrefixInterceptor{
+		re:          re,
+		intercepted: intercepted,
+		original:    original,
+		pr:          pr,
+		pw:          pw,
+	}
+	go interceptor.run()
+	return interceptor
+}
+
+// Write implements io.Writer. It writes into an internal pipe which the interceptor goroutine consumes.
+func (i *PrefixInterceptor) Write(p []byte) (int, error) {
+	// write into pipe; pipe writer preserves order
+	return i.pw.Write(p)
+}
+
+// Close stops the interceptor and closes the pipe.
+func (i *PrefixInterceptor) Close() error {
+	i.closeOnce.Do(func() {
+		// close the writer side which causes reader side to EOF
+		i.closeErr = i.pw.Close()
+		// ensure reader is drained (run goroutine will finish)
+		_ = i.pr.Close()
+	})
+	return i.closeErr
+}
+
+// run reads lines (and partial final chunk) and writes them.
+func (i *PrefixInterceptor) run() {
+	// Use a scanner but with a large buffer to handle long lines.
+	scanner := bufio.NewScanner(i.pr)
+	const maxTokenSize = 10 * 1024 * 1024
+	buf := make([]byte, 0, 64*1024)
+	scanner.Buffer(buf, maxTokenSize)
+
+	for scanner.Scan() {
+		line := scanner.Text() // note: newline removed
+		// re-append newline to preserve same output format
+		outLine := line + "\n"
+		if i.re.MatchString(line) {
+			_, _ = io.WriteString(i.intercepted, outLine)
+		}
+		_, _ = io.WriteString(i.original, outLine)
+	}
+	// handle any scanner error
+	if err := scanner.Err(); err != nil && err != io.EOF {
+		_, _ = fmt.Fprintf(i.original, "router scanner error: %v\n", err)
+	}
+}
--- xcpretty/xcpretty.go
+++ xcpretty/xcpretty.go
@@ -5,18 +5,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 
 	"github.com/bitrise-io/go-steputils/v2/ruby"
 	loggerV1 "github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/env"
 	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-io/go-xcode/v2/loginterceptor"
 	"github.com/bitrise-io/go-xcode/v2/xcodebuild"
 	"github.com/hashicorp/go-version"
 )
 
 const (
 	toolName = "xcpretty"
+	prefixed = `^[Bitrise.*].*`
 )
 
 // CommandModel ...
@@ -53,17 +56,19 @@ func (c CommandModel) PrintableCmd() string {
 
 // Run ...
 func (c CommandModel) Run() (string, error) {
-
 	// Configure cmd in- and outputs
 	pipeReader, pipeWriter := io.Pipe()
 
 	var outBuffer bytes.Buffer
 	outWriter := io.MultiWriter(&outBuffer, pipeWriter)
 
+	re := regexp.MustCompile(prefixed)
+	interceptor := loginterceptor.NewPrefixInterceptor(re, os.Stdout, outWriter)
+
 	xcodebuildCmd := c.xcodebuildCommand.Command(&command.Opts{
 		Stdin:  nil,
-		Stdout: outWriter,
-		Stderr: outWriter,
+		Stdout: interceptor,
+		Stderr: interceptor,
 	})
 
 	prettyCmd := c.Command(&command.Opts{
