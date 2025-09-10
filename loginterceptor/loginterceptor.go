966421569644a76631911cf3ac21434764374fec
Balázs Hajagos <balazs.hajagos@bitrise.io>
2025-09-10 19:55:48 +0200

    Make more concise non blocking writer

--- loginterceptor/loginterceptor.go
+++ loginterceptor/loginterceptor.go
@@ -2,11 +2,10 @@ package loginterceptor
 
 import (
 	"bufio"
-	"context"
+	"fmt"
 	"io"
 	"regexp"
 	"sync"
-	"time"
 
 	"github.com/bitrise-io/go-utils/v2/log"
 )
@@ -15,14 +14,13 @@ import (
 // both writers. Partial writes without newline are buffered until a newline.
 type PrefixInterceptor struct {
 	prefixRegexp *regexp.Regexp
-	intercepted  io.Writer
-	target       io.Writer
+	intercepted  *NonBlockingWriter
+	target       *NonBlockingWriter
 	logger       log.Logger
 
 	// internal pipe and goroutine to scan and route
 	internalReader *io.PipeReader
 	internalWriter *io.PipeWriter
-	writeTimeout   time.Duration
 
 	// close once
 	closeOnce sync.Once
@@ -31,27 +29,16 @@ type PrefixInterceptor struct {
 
 // NewPrefixInterceptor returns an io.WriteCloser. Writes are based on line prefix.
 func NewPrefixInterceptor(prefixRegexp *regexp.Regexp, intercepted, target io.Writer, logger log.Logger) *PrefixInterceptor {
-	return NewPrefixInterceptorWithTimeout(prefixRegexp, intercepted, target, logger, 1*time.Second)
-}
-
-// NewPrefixInterceptorWithTimeout returns an io.WriteCloser. Writes are based on line prefix.
-func NewPrefixInterceptorWithTimeout(prefixRegexp *regexp.Regexp, intercepted, target io.Writer, logger log.Logger, writeTimeout time.Duration) *PrefixInterceptor {
 	pipeReader, pipeWriter := io.Pipe()
 	interceptor := &PrefixInterceptor{
 		prefixRegexp:   prefixRegexp,
-		intercepted:    intercepted,
-		target:         target,
+		intercepted:    NewNonBlockingWriter(intercepted, logger),
+		target:         NewNonBlockingWriter(target, logger),
 		logger:         logger,
 		internalReader: pipeReader,
 		internalWriter: pipeWriter,
-		writeTimeout:   writeTimeout,
 	}
-
-	interceptCh := WriterWorker(interceptor.intercepted)
-	targetCh := WriterWorker(interceptor.target)
-
-	go interceptor.scan(interceptCh, targetCh)
-
+	go interceptor.run()
 	return interceptor
 }
 
@@ -65,29 +52,24 @@ func (i *PrefixInterceptor) Close() error {
 	i.closeOnce.Do(func() {
 		i.closeErr = i.internalWriter.Close()
 	})
-
 	return i.closeErr
 }
 
-func (i *PrefixInterceptor) scan(reqChIntercepted, reqChTarget chan<- writeReq) {
-	defer func() {
-		if err := i.internalReader.Close(); err != nil {
-			i.logger.Errorf("internal reader: %v", err)
-		}
-		// Close writers if able
-		if interceptedCloser, ok := i.intercepted.(io.Closer); ok {
-			if err := interceptedCloser.Close(); err != nil {
-				i.logger.Errorf("closing intercepted writer: %v", err)
-			}
-		}
-		if originalCloser, ok := i.target.(io.Closer); ok {
-			if err := originalCloser.Close(); err != nil {
-				i.logger.Errorf("closing original writer: %v", err)
-			}
-		}
-		defer close(reqChIntercepted) // stop the worker when done
-		defer close(reqChTarget)      // stop the worker when done
-	}()
+func (i *PrefixInterceptor) closeAfterRun() {
+	if err := i.intercepted.Close(); err != nil {
+		i.logger.Errorf("intercepted writer: %v", err)
+	}
+	if err := i.target.Close(); err != nil {
+		i.logger.Errorf("target writer: %v", err)
+	}
+	if err := i.internalReader.Close(); err != nil {
+		i.logger.Errorf("internal reader: %v", err)
+	}
+}
+
+// run reads lines (and partial final chunk) and writes them.
+func (i *PrefixInterceptor) run() {
+	defer i.closeAfterRun()
 
 	// Use a scanner but with a large buffer to handle long lines.
 	scanner := bufio.NewScanner(i.internalReader)
@@ -98,91 +80,71 @@ func (i *PrefixInterceptor) scan(reqChIntercepted, reqChTarget chan<- writeReq) 
 	for scanner.Scan() {
 		line := scanner.Text() // note: newline removed
 		// re-append newline to preserve same output format
-		msg := line + "\n"
+		outLine := line + "\n"
 
-		if i.prefixRegexp.MatchString(msg) {
-			ctx, cancel := context.WithTimeout(context.Background(), i.writeTimeout)
-			if _, err := WriteWithContext(ctx, reqChIntercepted, []byte(msg)); err != nil {
+		// Write to intercepted channel if matching regexp
+		if i.prefixRegexp.MatchString(line) {
+			if _, err := io.WriteString(i.intercepted, outLine); err != nil {
 				i.logger.Errorf("intercept writer error: %v", err)
 			}
-			cancel()
 		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), i.writeTimeout)
-		if _, err := WriteWithContext(ctx, reqChTarget, []byte(msg)); err != nil {
+		// Always write to target channel
+		if _, err := io.WriteString(i.target, outLine); err != nil {
 			i.logger.Errorf("writer error: %v", err)
 		}
-		cancel()
 	}
 
 	// handle any scanner error
 	if err := scanner.Err(); err != nil {
-		i.logger.Errorf("interceptor scanner error: %v\n", err)
+		i.logger.Errorf("router scanner error: %v\n", err)
 	}
 }
 
-// writeReq represents a single write request.
-type writeReq struct {
-	ctx  context.Context
-	data []byte
-	resp chan writeResp
+// NonBlockingWriter is an io.Writer that writes to a wrapped io.Writer in a non-blocking way.
+type NonBlockingWriter struct {
+	channel chan string
+	wrapped io.Writer
+	logger  log.Logger
 }
 
-type writeResp struct {
-	n   int
-	err error
+// NewNonBlockingWriter creates a new NonBlockingWriter.
+func NewNonBlockingWriter(w io.Writer, logger log.Logger) *NonBlockingWriter {
+	writer := &NonBlockingWriter{
+		channel: make(chan string, 100), // buffered channel to avoid blocking
+		wrapped: w,
+		logger:  logger,
+	}
+	go writer.Run()
+	return writer
 }
 
-// WriterWorker serializes writes to w. It returns a channel to submit requests.
-// Close the returned channel to stop the worker (it will finish current request and exit).
-func WriterWorker(w io.Writer) chan<- writeReq {
-	reqCh := make(chan writeReq)
-	go func() {
-		for req := range reqCh {
-			// perform the blocking write in a helper goroutine so we can honor req.ctx
-			done := make(chan writeResp, 1)
+// Write implements io.Writer. It writes into an internal pipe which the interceptor goroutine consumes.
+func (i *NonBlockingWriter) Write(p []byte) (int, error) {
+	select {
+	case i.channel <- string(p):
+		return len(p), nil
+	default:
+		return 0, fmt.Errorf("buffer full, dropping log")
+	}
+}
 
-			go func(r writeReq) {
-				n, err := w.Write(r.data)
-				done <- writeResp{n: n, err: err}
-			}(req)
+// Close stops the interceptor and closes the pipe.
+func (i *NonBlockingWriter) Close() error {
+	close(i.channel)
+	return nil
+}
 
-			select {
-			case <-req.ctx.Done():
-				// Caller gave up. We still need to wait for the underlying write goroutine
-				// to finish to avoid leaking it, but we return the ctx error to caller.
-				// Option A: just wait and discard result
-				go func() {
-					<-done // let the write goroutine finish and drop the result
-				}()
-				req.resp <- writeResp{n: 0, err: req.ctx.Err()}
-			case res := <-done:
-				// Write finished before context done.
-				req.resp <- res
-			}
-			close(req.resp)
+// Run consumes the channel and writes to the wrapped writer.
+func (i *NonBlockingWriter) Run() {
+	for msg := range i.channel {
+		if _, err := io.WriteString(i.wrapped, msg); err != nil {
+			i.logger.Errorf("NonBlockingWriter: wrapped writer error: %v", err)
 		}
-	}()
-	return reqCh
-}
-
-// WriteWithContext is a helper to submit a write and wait respecting ctx
-func WriteWithContext(ctx context.Context, reqCh chan<- writeReq, data []byte) (int, error) {
-	resp := make(chan writeResp, 1)
-	select {
-	case reqCh <- writeReq{ctx: ctx, data: data, resp: resp}:
-		// request submitted
-	case <-ctx.Done():
-		return 0, ctx.Err()
 	}
 
-	select {
-	case r := <-resp:
-		return r.n, r.err
-	case <-ctx.Done():
-		// If context expires after the request was submitted but before we read the resp,
-		// we still return ctx.Err(). The worker side will also return ctx.Err. to resp
-		// in the previous select if it sees ctx.Done() before write completes.
-		return 0, ctx.Err()
+	if closer, ok := i.wrapped.(io.Closer); ok {
+		if err := closer.Close(); err != nil {
+			i.logger.Errorf("NonBlockingWriter: closing wrapped writer: %v", err)
+		}
 	}
 }
--- loginterceptor/loginterceptor_test.go
+++ loginterceptor/loginterceptor_test.go
@@ -6,7 +6,6 @@ import (
 	"regexp"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-io/go-xcode/v2/loginterceptor"
@@ -54,7 +53,6 @@ func TestPrefixInterceptorWithPrematureClose(t *testing.T) {
 	msg4 := "Stuff [Bitrise Build Cache] Log message without prefix\n"
 
 	go func() {
-
 		_, _ = sut.Write([]byte(msg1))
 		_, _ = sut.Write([]byte(msg2))
 		_, _ = sut.Write([]byte(msg3))
@@ -68,12 +66,12 @@ func TestPrefixInterceptorWithPrematureClose(t *testing.T) {
 	assert.Equal(t, msg1+msg2+msg3, string(target))
 }
 
-func TestPrefixInterceptorWhenWritersAreBlocking(t *testing.T) {
-	blockingWriter := newBlockingWriter()
+func TestPrefixInterceptorWithBlockedPipe(t *testing.T) {
+	interceptReader, interceptWriter := io.Pipe()
 	targetReader, targetWriter := io.Pipe()
 	re := regexp.MustCompile(`^\[Bitrise.*\].*`)
 
-	sut := loginterceptor.NewPrefixInterceptorWithTimeout(re, blockingWriter, targetWriter, log.NewLogger(), 50*time.Millisecond)
+	sut := loginterceptor.NewPrefixInterceptor(re, interceptWriter, targetWriter, log.NewLogger())
 
 	msg1 := "Log message without prefix\n"
 	msg2 := "[Bitrise Analytics] Log message with prefix\n"
@@ -85,57 +83,19 @@ func TestPrefixInterceptorWhenWritersAreBlocking(t *testing.T) {
 		defer sut.Close()
 
 		_, _ = sut.Write([]byte(msg1))
-		time.Sleep(10 * time.Millisecond)
-		blockingWriter.Allow()
 		_, _ = sut.Write([]byte(msg2))
-		time.Sleep(10 * time.Millisecond)
-		blockingWriter.Allow()
 		_, _ = sut.Write([]byte(msg3))
-		time.Sleep(10 * time.Millisecond)
-		blockingWriter.Allow()
 		_, _ = sut.Write([]byte(msg4))
-		time.Sleep(10 * time.Millisecond)
-		blockingWriter.Allow()
 	}()
 
 	target, err := io.ReadAll(targetReader)
 	assert.NoError(t, err)
-	assert.Equal(t, msg2+msg3, string(blockingWriter.buf))
 	assert.Equal(t, msg1+msg2+msg3+msg4, string(target))
-}
 
-func TestPrefixInterceptorWhenWriteIsSlowerThenTimeout(t *testing.T) {
-	blockingWriter := newBlockingWriter()
-	targetReader, targetWriter := io.Pipe()
-	re := regexp.MustCompile(`^\[Bitrise.*\].*`)
-
-	sut := loginterceptor.NewPrefixInterceptorWithTimeout(re, blockingWriter, targetWriter, log.NewLogger(), 10*time.Millisecond)
-
-	msg1 := "Log message without prefix\n"
-	msg2 := "[Bitrise Analytics] Log message with prefix\n"
-	msg3 := "[Bitrise Build Cache] Log message with prefix\n"
-	msg4 := "Stuff [Bitrise Build Cache] Log message without prefix\n"
-
-	go func() {
-		//nolint:errCheck
-		defer sut.Close()
-
-		_, _ = sut.Write([]byte(msg1))
-		time.Sleep(30 * time.Millisecond)
-		blockingWriter.Allow()
-		_, _ = sut.Write([]byte(msg2))
-		time.Sleep(30 * time.Millisecond)
-		blockingWriter.Allow()
-		_, _ = sut.Write([]byte(msg3))
-		time.Sleep(30 * time.Millisecond)
-		blockingWriter.Allow()
-		_, _ = sut.Write([]byte(msg4))
-		time.Sleep(30 * time.Millisecond)
-		blockingWriter.Allow()
-	}()
-
-	_, err := io.ReadAll(targetReader)
+	// Reading from interceptReader should block until targetWriter is read
+	intercepted, err := io.ReadAll(interceptReader)
 	assert.NoError(t, err)
+	assert.Equal(t, msg2+msg3, string(intercepted))
 }
 
 func readTwo(r1, r2 io.Reader) (out1, out2 []byte, err error) {
@@ -167,43 +127,4 @@ func readTwo(r1, r2 io.Reader) (out1, out2 []byte, err error) {
 		return b1.Bytes(), b2.Bytes(), e2
 	}
 	return b1.Bytes(), b2.Bytes(), nil
-}
-
-// blockingWriter blocks on each Write until an allow signal is received.
-type blockingWriter struct {
-	allow  chan struct{}
-	buf    []byte
-	mu     sync.Mutex
-	closed bool
-}
-
-func newBlockingWriter() *blockingWriter {
-	return &blockingWriter{allow: make(chan struct{})}
-}
-
-func (w *blockingWriter) Write(p []byte) (int, error) {
-	// block until allowed
-	<-w.allow
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	if w.closed {
-		return 0, io.ErrClosedPipe
-	}
-	w.buf = append(w.buf, p...)
-	return len(p), nil
-}
-
-func (w *blockingWriter) Allow() {
-	// Non-blocking safe: close or send depending on usage.
-	// For repeated allows, use a buffered channel or recreate it.
-	select {
-	case w.allow <- struct{}{}:
-	default:
-	}
-}
-
-func (w *blockingWriter) Close() {
-	w.mu.Lock()
-	w.closed = true
-	w.mu.Unlock()
 }
