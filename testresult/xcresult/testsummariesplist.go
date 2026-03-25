6eac0ca1ea2f2c7faffed754232d9712ed889060
Gergely Sallai <gergely-sallai@users.noreply.github.com>
2026-03-25 18:08:03 +0100

    Add xcresult converter from deploy step (#315)
    * refact: STEP-2230 Add xcresult converter from deploy step
    
    - Moved over XCResult converters from steps-deploy-to-bitrise
    - Bumped dependency versions
    
    * style: STEP-2230 Fix lint
    
    Added mandatory docs for exported functions and variables. Did not check for the validity of exports, though.
    
    * build: STEP-2230 Fix failing tests
    
    Missing test artifacts.
    
    * refact: STEP-2230 Move test logic entirely to go test
    
    - Removed the added bitrise.yml config that prepared the test environment, because that way `go test` would heavily depend on bitrise ci config.
    - Moved test data preparation logic to test_converter
    
    * fix: STEP-2230 Decouple test from local timezone
    
    This test was failing when it is run on a machine that has different timezone than UTC.
    The reason is that we were using xcrun xcresulttool to extract attachments from xcresult and that assembles filenames on the fly using local TZ, this made the test prone to failures (false negative) because it explicitly verified for UTC times.
    
    I don't think we need to test xcresult, so refactored this part.
    
    * refact: STEP-2230 private visibility in xcresult
    
    * refact: STEP-2230 private visibility in xcresult3
    
    Note: had to rename some variable to not be confused with private types now.
    This also true for `func Parse(....)` -> `func loadXCResultData(....)` it was clashing with converter.go's `parse()`
    
    * style: STEP-2230 Adding missing docs.
    
    This has to stay exported, was too eager to remove its doc.
    
    * chore: update to new go-xcode v1
    
    * feat!: STEP-2230 Remove Setup from Converter interface
    
    Reasons:
    - This was no-op for most of our converters
    - The parameter (the reason the func was introduced in the first place I assume) is closely tied to xcresult3 conversion, which is a close implementation detail, not something that belongs in a general interface.
    
    * fix: STEP-2230 Revert making test.Subtest private
    
    It was causing test failures in deploy-to-bitrise-io step during plist parsing
    
    * chore: Updated deps and mocks
    
    * chore: lint fix
    
    ---------
    
    Co-authored-by: Gergely Sallai <1516628+gergely-sallai@users.noreply.github.com>

---
+++ testresult/xcresult/testsummariesplist.go
@@ -0,0 +1,112 @@
+package xcresult
+
+import (
+	"fmt"
+	"strings"
+)
+
+type testSummaryPlist struct {
+	FormatVersion     string
+	TestableSummaries []testableSummary
+}
+
+func collapsesubtestTree(data subtests) (tests subtests) {
+	for _, test := range data {
+		if len(test.Subtests) > 0 {
+			tests = append(tests, collapsesubtestTree(test.Subtests)...)
+		}
+		if test.TestStatus != "" {
+			tests = append(tests, test)
+		}
+	}
+	return
+}
+
+func (summaryPlist testSummaryPlist) tests() ([]string, map[string]subtests) {
+	var keyOrder []string
+	tests := map[string]subtests{}
+	var subTests subtests
+	for _, testableSummary := range summaryPlist.TestableSummaries {
+		for _, test := range testableSummary.Tests {
+			subTests = append(subTests, collapsesubtestTree(test.Subtests)...)
+		}
+	}
+	for _, test := range subTests {
+		// TestIdentifier is in a format of testID/testCase
+		testID := strings.Split(test.TestIdentifier, "/")[0]
+		if _, found := tests[testID]; !found {
+			keyOrder = append(keyOrder, testID)
+		}
+		tests[testID] = append(tests[testID], test)
+	}
+	return keyOrder, tests
+}
+
+type test struct {
+	Subtests subtests
+}
+
+type testableSummary struct {
+	TargetName      string
+	TestKind        string
+	TestName        string
+	TestObjectClass string
+	Tests           []test
+}
+
+type failureSummary struct {
+	FileName           string
+	LineNumber         int
+	Message            string
+	PerformanceFailure bool
+}
+
+type subtest struct {
+	Duration         float64
+	TestStatus       string
+	TestIdentifier   string
+	TestName         string
+	TestObjectClass  string
+	Subtests         subtests
+	FailureSummaries []failureSummary
+}
+
+func (st subtest) failure() (message string) {
+	prefix := ""
+	for _, failure := range st.FailureSummaries {
+		message += fmt.Sprintf("%s%s:%d - %s", prefix, failure.FileName, failure.LineNumber, failure.Message)
+		prefix = "\n"
+	}
+	return
+}
+
+func (st subtest) skipped() bool {
+	return st.TestStatus == "Skipped"
+}
+
+type subtests []subtest
+
+func (sts subtests) failuresCount() (count int) {
+	for _, test := range sts {
+		if len(test.FailureSummaries) > 0 {
+			count++
+		}
+	}
+	return count
+}
+
+func (sts subtests) skippedCount() (count int) {
+	for _, test := range sts {
+		if test.skipped() {
+			count++
+		}
+	}
+	return count
+}
+
+func (sts subtests) totalTime() (time float64) {
+	for _, test := range sts {
+		time += test.Duration
+	}
+	return time
+}
